a097060a0ae6e8bf6edb5cbcfe7cd09bbea87a65
SangyYuhiter <sangyu.code@gmail.com>
2023-03-05 02:17:29 +0800

    add Test option

---
+++ inc/CommonIncs.h
@@ -0,0 +1,17 @@
+#ifndef COMMONINCS_H
+#define COMMONINCS_H
+
+// for C header files
+
+// for C++ containers
+#include <map>
+#include <vector>
+
+// for C++ Input/Output
+
+// for C++ Multi-threading
+
+// for C++ othres
+#include <string>
+
+#endif
--- test/Test.cpp
+++ test/Test.cpp
@@ -6,21 +6,62 @@
  @Description  :
  ************************************************************************/
 
+#include "CommonIncs.h"
 #include "LogUtils.h"
 #include "TestAdd.h"
 #include "TestEnumUtils.h"
 #include "TestLogUtils.h"
 
+enum class TestType
+{
+    TEST_TYPE_INVALID = -1,
+    TEST_LOG_UTILS,
+    TEST_ENUM_UTILS,
+    TEST_ADD,
+    TEST_TYPE_NUM,
+};
+
+std::vector<std::string> testTypeStrings = {
+    "TEST_LOG_UTILS",
+    "TEST_ENUM_UTILS",
+    "TEST_ADD",
+    "TEST_TYPE_NUM",
+};
+
 int main()
 {
     PPS_LOG_ENTRY();
-    PPS_LOG_INFO("%zu", sizeof(unsigned long int) * 4);
 
-    testLogUtils();
+    PPS_LOG_INFO("Please select a option as below:");
+    for (int i = static_cast<int>(TestType::TEST_LOG_UTILS);
+         i < static_cast<int>(TestType::TEST_TYPE_NUM); ++i)
+    {
+        PPS_LOG_INFO("%d------%s", i, testTypeStrings[i].c_str());
+    }
+    int selectType;
+    PPS_LOG_INFO("input option:");
+    scanf("%d", &selectType);
+    if (selectType <= static_cast<int>(TestType::TEST_TYPE_INVALID) ||
+        selectType >= static_cast<int>(TestType::TEST_TYPE_NUM))
+    {
+        PPS_LOG_ERROR("ERROR input option:%d,will route to TEST_LOG", selectType);
+        selectType = static_cast<int>(TestType::TEST_LOG_UTILS);
+    }
 
-    testEnumUtils();
-
-    testAdd();
+    switch (static_cast<TestType>(selectType))
+    {
+        case TestType::TEST_LOG_UTILS:
+            testLogUtils();
+            break;
+        case TestType::TEST_ENUM_UTILS:
+            testEnumUtils();
+            break;
+        case TestType::TEST_ADD:
+            testAdd();
+            break;
+        default:
+            break;
+    }
 
     PPS_LOG_EXIT();
     return 0;
